aa45e82969e88933dca1b8c127c972b62e5d9b40
Chedro Cardenas <chegiorca@hotmail.com>
2021-01-31 16:49:31 -0700

    Add endpoint for Author

--- aspnet_api_store/StoreApi.Service/RDTRepo.cs
+++ aspnet_api_store/StoreApi.Service/RDTRepo.cs
@@ -21,7 +21,8 @@ namespace StoreApi.Service
         {
             return _ctx.Topics;
         }
-
+// WORK IN THIS!! all the articles that include the given string in the search box
+// on the MVC ...dynamic typing
         public IEnumerable<Article> GetArticlesByGivenTitle(string title)
         {
             return _ctx.Articles.Where(a => a.Title == title);
@@ -32,9 +33,10 @@ namespace StoreApi.Service
             return _ctx.Articles.Include(a=>a.Topic).Include(a => a.Author);
         }
 
-        public IEnumerable<Article> GetArticlesByGivenEmail()
+        public IEnumerable<Article> GetArticlesByGivenEmail(string email)
         {
-            return null;
+          //  Recieved an email and given the article that the AUTHOR wrote
+            return _ctx.Articles.Where(a => a.Author.Email == email).Include(a => a.Author).Include(a => a.Topic);
         }
 
         public void CreateArticle(Article article)
@@ -72,9 +74,15 @@ namespace StoreApi.Service
           return _ctx.Readers;
         }
 
-        public void GetAuthorIfValidCredential()
+        public Author GetAuthorIfValidCredential(Author author)
         {
-
+          //The author will be NULL if doesnt exist or password doesnt match
+          var isEmailOk =_ctx.Authors.FirstOrDefault(a => a.Email == author.Email);
+          if( isEmailOk != null){
+            var isPasswordOk = _ctx.Authors.FirstOrDefault(ax =>ax.Password == author.Password);
+            return isPasswordOk != null? isPasswordOk: null;
+          }
+          return isEmailOk;
         }
 
         public bool CheckIfReaderExists(Reader reader)
@@ -84,6 +92,20 @@ namespace StoreApi.Service
           return EmailExists != null || UserExists != null? true : false;
 
         }
+
+         public Reader GetReaderIfValidCredential(Reader reader)
+        {
+          //The author will be NULL if doesnt exist or password doesnt match
+          var isEmailOk =_ctx.Readers.FirstOrDefault(a => a.Email == reader.Email);
+          if( isEmailOk == null){
+            return isEmailOk;
+          }
+          var isPasswordOk = _ctx.Readers.FirstOrDefault(ax =>ax.Password == isEmailOk.Password);
+          return isPasswordOk;
+        }
+
+
+// LIKE ARTICLE
 
 
 
