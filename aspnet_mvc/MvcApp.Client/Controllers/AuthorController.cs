7bdcb6bfda0d76902c23e39d1530e62a9431d51d
Joshwin Greene <joshwin.greene@gmail.com>
2021-01-30 16:57:47 -0800

    Added foundational login and author home code and (temp) author article viewer view code

--- aspnet_mvc/MvcApp.Client/Controllers/AuthorController.cs
+++ aspnet_mvc/MvcApp.Client/Controllers/AuthorController.cs
@@ -6,15 +6,47 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+
 namespace MvcApp.Client.Controllers
 {
   [Route("[controller]")] // route parser
-  public class AuthorController : Controller // test change
+  public class AuthorController : Controller
   {
     private string apiUrl = "https://localhost:5001/";
     private HttpClient _http = new HttpClient();
 
     [HttpGet]
+    public IActionResult Home()
+    {
+        ViewBag.Title = "Login";
+        return View("Home");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(AuthorViewModel authorViewModel)
+    {
+        System.Console.WriteLine("Login");
+
+        System.Console.WriteLine("Email: " + authorViewModel.Email);
+        System.Console.WriteLine("Password: " + authorViewModel.Password);
+
+        return View("AuthorMain", new List<ArticleViewModel> { 
+          new ArticleViewModel("First", false),
+          new ArticleViewModel("Second", false),
+          new ArticleViewModel("Third", true),
+          new ArticleViewModel("Four", true),
+          new ArticleViewModel("Five", true),
+          new ArticleViewModel("Six", true),
+        });
+    }
+
+    [HttpGet("view_article")]
+    public IActionResult ViewArticle()
+    {
+      return View("AuthorArticleViewer", "tempValue");
+    }
+
+    [HttpGet("temp")]
     public async Task<IActionResult> Get()
     {
       var response = await _http.GetAsync(apiUrl + "Topic/topics");
@@ -28,7 +60,7 @@ namespace MvcApp.Client.Controllers
         var ObjOrderList = JsonConvert.DeserializeObject<List<TopicViewModel>>(jsonResponse);
         //ObjOrderList.ForEach(m => System.Console.WriteLine(m.Name));
 
-        return await Task.FromResult(View("home", ObjOrderList));
+        return await Task.FromResult(View("TopicList", ObjOrderList));
         // return View("home", ObjOrderList);
       }
       // return Task.FromResult(View("Error"));
