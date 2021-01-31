45a93de7d315106853e7514518aabadaeeb6f50b
elliott lockwood <lockwood721@gmail.com>
2021-01-30 19:38:45 -0600

    get articles endpoint test

--- aspnet_api_store/StoreApi.Service/RDTRepo.cs
+++ aspnet_api_store/StoreApi.Service/RDTRepo.cs
@@ -1,4 +1,54 @@
+using StoreApi.Domain.Models; 
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+
 namespace StoreApi.Service
 {
-    //test 
+    public class RDTRepo 
+    {
+        private RDTContext _ctx;
+        //Constructor
+
+        public RDTRepo(RDTContext context)
+        {
+            _ctx = context;
+        }
+
+        public IEnumerable<Topic> GetTopics()
+        {
+            return _ctx.Topics;
+        }
+
+        public IEnumerable<Article> GetArticlesByGivenTitle(string title)
+        {
+            return _ctx.Articles.Where(a => a.Title == title); 
+        }
+
+        public IEnumerable<Article>  GetArticles()
+        {
+            return _ctx.Articles; 
+        }
+
+        public IEnumerable<Article> GetArticlesByGivenEmail()
+        {
+            return null;
+        }
+
+        public void CreateArticle(Article article)
+        {
+            _ctx.Articles.Add(article); 
+            _ctx.SaveChanges();
+        }
+
+
+
+
+
+        
+
+    
+    }
 }
