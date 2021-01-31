be137856f021f97c63df1c9615f5d02df58678aa
chegior <chegiorca@hotmail.com>
2021-01-30 19:15:36 -0700

    reader list vew, SSL for controllers added on http pass httphandler see constructor on CONTROLLERS

--- aspnet_mvc/MvcApp.Client/Controllers/AuthorController.cs
+++ aspnet_mvc/MvcApp.Client/Controllers/AuthorController.cs
@@ -3,36 +3,37 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using MvcApp.Client.Models.Author;
 using System.Collections.Generic;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+
 namespace MvcApp.Client.Controllers
 {
   [Route("[controller]")] // route parser
-  public class AuthorController : Controller
+  public class AuthorController : Controller // test change
   {
     private string apiUrl = "https://localhost:5001/";
-    private HttpClient _http = new HttpClient();
+    private HttpClient _http;
+
+    public AuthorController(){
+
+      HttpClientHandler clientHandler = new HttpClientHandler();
+      clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+      _http = new HttpClient(clientHandler);
+    }
+
 
     [HttpGet]
     public async Task<IActionResult> Get()
     {
       var response = await _http.GetAsync(apiUrl + "Topic/topics");
 
-      if (response.IsSuccessStatusCode) // IsSuccessStatusCode
+      if (response.IsSuccessStatusCode)
       {
-        //var content = Json.Convert<TopicViewModel>(await response.Content.ReadStringAsync());
         var jsonResponse = await response.Content.ReadAsStringAsync();
-        System.Console.WriteLine(jsonResponse);
-        
-        var ObjOrderList = JsonConvert.DeserializeObject<List<TopicViewModel>>(jsonResponse);
-        //ObjOrderList.ForEach(m => System.Console.WriteLine(m.Name));
+        // System.Console.WriteLine(jsonResponse);
 
+        var ObjOrderList = JsonConvert.DeserializeObject<List<TopicViewModel>>(jsonResponse);
         return await Task.FromResult(View("home", ObjOrderList));
-        // return View("home", ObjOrderList);
       }
-      // return Task.FromResult(View("Error"));
-      // asdasd
       return View("Error");
     }
   }
---
+++ aspnet_mvc/MvcApp.Client/Models/Reader/ArticleViewModel.cs
@@ -0,0 +1,11 @@
+namespace MvcApp.Client.Models.Reader
+{
+    public class ArticleViewModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public string ImagePath { get; set; }
+        public string Topic { get; set; }
+    }
+}
