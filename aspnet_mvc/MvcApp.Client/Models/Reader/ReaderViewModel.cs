4c2dabfa96a44265f5f72958a34d8f47109c4c14
Chedro Cardenas <chegiorca@hotmail.com>
2021-02-03 01:56:29 -0700

    Not able to add Topic Navigation

--- aspnet_mvc/MvcApp.Client/Models/Reader/ReaderViewModel.cs
+++ aspnet_mvc/MvcApp.Client/Models/Reader/ReaderViewModel.cs
@@ -1,4 +1,5 @@
 // Username email password
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcApp.Client.Models.Reader
@@ -9,5 +10,6 @@ namespace MvcApp.Client.Models.Reader
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public List<string> Topics { get; set; }
     }
 }
---
+++ aspnet_mvc/MvcApp.Client/Models/Shared/ViewTopicModel.cs
@@ -0,0 +1,11 @@
+namespace MvcApp.Client.Models.Shared
+{
+  public class ViewTopicModel
+  {
+    public long EntityId { get; set; }
+    public string Name { get; set; }
+
+  }
+}
+
+//FLIP THE NAME FOR USING AS A SHARED TOPIC vs READER AND AUTHOR
